8b3d11c15b7b25188e79f078f1cbaf902f309a8f
Oleou <o.utekov@gmail.com>
2014-10-25 00:29:51 +0400

    cc

---
+++ Webdj/Webdj/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Юыху\Projects\robodraw\Webdj\Webdj\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Webdj/Webdj/env/Scripts/easy_install-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\Юыху\Projects\robodraw\Webdj\Webdj\env\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==6.0.2','console_scripts','easy_install'
+__requires__ = 'setuptools==6.0.2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('setuptools==6.0.2', 'console_scripts', 'easy_install')()
+    )
